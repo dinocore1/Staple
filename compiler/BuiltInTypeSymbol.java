4ddd7efcb3906ee3593e08f080002b7b9323f352
Paul Soucy <paul@dev-smart.com>
2012-12-08 14:16:25 -0500

    starting rewrite

--- compiler/BuiltInTypeSymbol.java
+++
@@ -1,5 +0,0 @@
-/** A symbol to represent built in types such int, float primitive types */
-public class BuiltInTypeSymbol extends Symbol implements Type {
-    public BuiltInTypeSymbol(String name) { super(name); }
-
-}
